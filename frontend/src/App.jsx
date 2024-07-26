41d2195c607aa61b40e074d56227cd982c268f4f
shadowind797 <shadowind797@gmail.com>
2024-07-26 17:58:15 +0500

    modified:   backend/api/migrations/0001_initial.py 	new file:   backend/api/migrations/0002_category_alter_user_phone_alter_user_status_item.py 	new file:   backend/api/urls.py 	new file:   frontend/.env 	new file:   frontend/.eslintrc.cjs 	new file:   frontend/.gitignore 	new file:   frontend/README.md 	new file:   frontend/index.html 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/vite.svg 	new file:   frontend/src/App.jsx 	new file:   frontend/src/api.js 	new file:   frontend/src/assets/react.svg 	new file:   frontend/src/components/Form.jsx 	new file:   frontend/src/components/Item.jsx 	new file:   frontend/src/components/ProtectedRoute.jsx 	new file:   frontend/src/constants.js 	new file:   frontend/src/main.jsx 	new file:   frontend/src/pages/404.jsx 	new file:   frontend/src/pages/Home.jsx 	new file:   frontend/src/pages/Login.jsx 	new file:   frontend/src/pages/Register.jsx 	new file:   frontend/src/pages/SearchFood.jsx 	new file:   frontend/vite.config.js 	new file:   package-lock.json 	modified:   backend/api/__pycache__/models.cpython-312.pyc 	modified:   backend/api/__pycache__/serializers.cpython-312.pyc 	modified:   backend/api/__pycache__/views.cpython-312.pyc 	modified:   backend/api/migrations/__pycache__/0001_initial.cpython-312.pyc 	modified:   backend/api/models.py 	modified:   backend/api/serializers.py 	modified:   backend/api/urls.py 	modified:   backend/api/views.py 	modified:   backend/backend/__pycache__/settings.cpython-312.pyc 	modified:   backend/backend/__pycache__/urls.cpython-312.pyc 	modified:   backend/backend/settings.py 	modified:   backend/backend/urls.py 	modified:   frontend/.env 	modified:   frontend/src/App.jsx 	modified:   frontend/src/api.js 	modified:   frontend/src/components/Form.jsx 	modified:   frontend/src/components/Item.jsx 	modified:   frontend/src/components/ProtectedRoute.jsx 	modified:   frontend/src/constants.js 	modified:   frontend/src/pages/404.jsx 	modified:   frontend/src/pages/Home.jsx 	modified:   frontend/src/pages/Login.jsx 	modified:   frontend/src/pages/Register.jsx 	modified:   frontend/src/pages/SearchFood.jsx 	backend/api/__pycache__/urls.cpython-312.pyc 	backend/api/migrations/__pycache__/0002_category_alter_user_phone_alter_user_status_item.cpython-312.pyc 	backend/backend/__pycache__/wsgi.cpython-312.pyc

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import react from "react";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import Login from "./pages/Login.jsx"
+import Register from "./pages/Register.jsx"
+import Home from "./pages/Home.jsx"
+import NotFound from "./pages/404.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login/" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="login/" element={<Login />} />
+        <Route path="register/" element={<RegisterAndLogout />} />
+        <Route path="logout/" element={<Logout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
