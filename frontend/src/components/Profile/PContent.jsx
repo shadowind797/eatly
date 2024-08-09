c1dfc08d3da49111a086005083a0947c282d6789
shadowind797 <shadowind797@gmail.com>
2024-08-09 15:49:46 +0500

    Start work with Profile page

--- frontend/src/components/Profile/PContent.jsx
+++ frontend/src/components/Profile/PContent.jsx
@@ -1,7 +1,37 @@
 function PContent() {
     return (
-        <div>
-
+        <div id="profile-content-profile">
+            <div id="profile">
+                <div id="profile-info">
+                    <div id="photo-name">
+                        <img src="" alt=""/>
+                        <div>
+                            <h3>Username</h3>
+                            <p>Name</p>
+                        </div>
+                    </div>
+                    <div id="others">
+                        <p>ID</p>
+                        <p>Phone</p>
+                        <p>email</p>
+                    </div>
+                </div>
+                <div id="profile-actions">
+                    <button>
+                        <img src="" alt="Change password" />
+                        <p>Change password</p>
+                    </button>
+                    <button>
+                        <img src="" alt="Log Out" />
+                        <p>Log Out</p>
+                    </button>
+                    <button>
+                        <img src="" alt="Delete account" />
+                        <p>Delete account</p>
+                    </button>
+                </div>
+            </div>
+            <div></div>
         </div>
     )
 }
